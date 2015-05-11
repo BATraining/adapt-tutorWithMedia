eb17812911578d559ef868605143a3ffc64aa0da
Jerome Lam <jerome.lam@gmail.com>
2015-05-11 18:38:51 -0400

    Update license and copyright notice.

--- js/adapt-tutor-withMedia.js
+++ js/adapt-tutor-withMedia.js
@@ -1,9 +1,20 @@
 /*
  * adapt-tutor-withMedia
- * License - http://github.com/adaptlearning/adapt_framework/blob/master/LICENSE
- * Maintainers - Kevin Corry <kevinc@learningpool.com>, Daryl Hedley <darylhedley@hotmail.com>,
- *              Himanshu Rajotia <himanshu.rajotia@exultcorp.com>,
-                Faisal Patel <shahfaisal.patel@exultcorp.com>
+ * Copyright (C) 2015 Bombardier Inc. (www.batraining.com)
+ * https://github.com/BATraining/adapt-tutor-withMedia/blob/master/LICENSE
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 define(function(require) {
 
